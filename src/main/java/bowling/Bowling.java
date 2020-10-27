db63cd4beebc479d055467e055046a9574e041d9
Dreamy.WJY <1104195224@qq.com>
2020-10-27 16:12:44 +0800

    Update

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -1,32 +1,32 @@
 package bowling;
 
 public class Bowling {
-	
-	int[] rolls = new int[21];
-	int rollIndex = 0;
 
-	public void roll(int pinsKnockedDown) {
-		rolls[rollIndex++] = pinsKnockedDown;
-	}
+    int[] rolls = new int[21];
+    int rollIndex = 0;
 
-	public int getScore() {
+    public void roll(int pinsKnockedDown) {
+        rolls[rollIndex++] = pinsKnockedDown;
+    }
+
+    public int getScore() {
         int sum = 0;
         int rollsIndex = 0;
         int total = 10;
 
-		for (int frame = 0; frame < total; frame++) {
-			if(rolls[rollsIndex] == 10) {
-				sum += 10 + rolls[rollsIndex + 1] + rolls[rollsIndex + 2];
-				rollsIndex += 1;
-			} else if (rolls[rollsIndex] + rolls[rollsIndex + 1] == 10) {
-				sum += 10 + rolls[rollsIndex + 2];
-				rollsIndex += 2;
-			} else {
-				sum += rolls[rollsIndex] + rolls[rollsIndex + 1];
-				rollsIndex += 2;
-			}
-		}
+        for (int frame = 0; frame < total; frame++) {
+            if (rolls[rollsIndex] == 10) {
+                sum += 10 + rolls[rollsIndex + 1] + rolls[rollsIndex + 2];
+                rollsIndex += 1;
+            } else if (rolls[rollsIndex] + rolls[rollsIndex + 1] == 10) {
+                sum += 10 + rolls[rollsIndex + 2];
+                rollsIndex += 2;
+            } else {
+                sum += rolls[rollsIndex] + rolls[rollsIndex + 1];
+                rollsIndex += 2;
+            }
+        }
 
-		return sum;
-	}
+        return sum;
+    }
 }
